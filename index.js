9458af896ab762d28733a20015bfb2048b42c7ca
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-27 20:33:08 +0600

    services id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +8,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
 app.use(express.json());
-
-// console.log(process.env.DB_PASS);
-// console.log(process.env.DB_USER);
-
-
 
 
 
@@ -30,6 +25,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const carCollection = client.db('carDoctors').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const cursor = carCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                projection: { title: 1,title: 1, service_id: 1, price: 1 },
+            };
+
+            const result = await carCollection.findOne(query, options);
+            res.send(result);
+        })
+
+
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
