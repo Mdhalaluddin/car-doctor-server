166a51d28e290fe4dc09d7f6b6ecba482cf64a87
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-31 01:07:33 +0600

    forbidden access

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -8,13 +9,27 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-    origin: ['http://localhost:5173/'], 
+    origin: ['http://localhost:5173'], 
     credentials: true 
 }))
 app.use(express.json());
+app.use(cookieParser())
 
 
 
+const verifyToken = async(req, res, next)=>{
+    const token = req.cookies?.token;
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user= decoded;
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3x6azjv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -46,7 +61,7 @@ async function run() {
         })
 
         // services related api
-        app.get('/services', async (req, res) => {
+        app.get('/services',  async (req, res) => {
             const cursor = carCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -64,8 +79,13 @@ async function run() {
             res.send(result);
         })
         // car service booking section
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, async (req, res) => {
             console.log(req.query.email);
+            // console.log('tok tok token', req.cookies.token);
+            console.log('user in the valid token', req.user);
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -74,7 +94,7 @@ async function run() {
             res.send(result)
         })
 
-        app.post("/bookings", async (req, res) => {
+        app.post("/bookings", verifyToken, async (req, res) => {
             const booking = req.body;
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
